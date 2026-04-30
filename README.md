852519dacc6a256a541d0da92c9be4a259126403
LichaAdeba <127605835+LichaAdeba@users.noreply.github.com>
2026-04-30 19:44:44 +0000

    Commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab_4
+
+#first step 
+gcc main.c sum_array.s -o program
+
+#second step
+./program data.txt
--- main.c
+++ main.c
@@ -1,16 +1,12 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
-
 extern int sum_array(int *arr, int n);
 
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     FILE *file;
     int number;
-    int n;
+
     if (argc != 2) {
         printf("Usage: %s filename\n", argv[0]);
         return 1;
@@ -22,25 +18,27 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    if (fscanf(file, "%d", &n) != 1) {
-        printf("Invalid file format\n");
-        return 1;
-    }
+    int capacity = 10;
+    int size = 0;
+    int *arr = malloc(capacity * sizeof(int));
 
-    int *arr = malloc(n * sizeof(int));
-
-    for (int i = 0; i < n; i++) {
-        if (fscanf(file, "%d", &arr[i]) != 1) {
-            printf("Error reading number\n");
-            return 1;
+    while (fscanf(file, "%d", &number) == 1) {
+        if (size >= capacity) {
+            capacity *= 2;
+            arr = realloc(arr, capacity * sizeof(int));
+            if (arr == NULL) {
+                printf("Memory allocation failed\n");
+                return 1;
+            }
         }
+        arr[size++] = number;
     }
-    fclose(file);
-    int result = sum_array(arr, n);
-    printf("Sum = %d\n", result);
-    free(arr);
 
+    fclose(file);
+
+    int result = sum_array(arr, size);
+    printf("Sum = %d\n", result);
+
+    free(arr);
     return 0;
 }
-
-
